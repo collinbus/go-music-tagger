bdb82cdb20f8b8d87114eb89b2ee522a99052609
Collin <collin.busschots4@gmail.com>
2020-03-30 00:56:12 +0200

    Merge file and file reader

--- pkg/flac/file.go
+++ pkg/flac/file.go
@@ -1,5 +1,12 @@
 package flac
 
+import (
+	"errors"
+	"io/ioutil"
+)
+
+var flacFileIdentifier = []byte{0x66, 0x4C, 0x61, 0x43}
+
 type File struct {
 	StreamInfo *StreamInfo
 	Size       int
@@ -7,4 +14,44 @@ type File struct {
 
 func NewFile(streamInfo *StreamInfo, size int) *File {
 	return &File{StreamInfo: streamInfo, Size: size}
+}
+
+type FileReader struct {
+	streamInfoReader StreamInfoReader
+}
+
+func (fr *FileReader) ReadFile(path string) (*File, error) {
+	fileBytes, err := ioutil.ReadFile(path)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if !isFlacFile(fileBytes) {
+		return nil, errors.New("file at " + path + " is not a flac file")
+	}
+
+	info := fr.streamInfoReader.NewStreamInfo(fileBytes[8:42])
+
+	flacFile := NewFile(&info, len(fileBytes))
+	return flacFile, nil
+}
+
+func isFlacFile(data []byte) bool {
+	if len(data) < 4 {
+		return false
+	}
+
+	for i := range flacFileIdentifier {
+		if data[i] != flacFileIdentifier[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func NewFileReader(reader StreamInfoReader) *FileReader {
+	return &FileReader{
+		streamInfoReader: reader,
+	}
 }
