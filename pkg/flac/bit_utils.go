344ceb4441d55976710a3ec7a760ea54409ebb50
Collin <collin.busschots4@gmail.com>
2020-03-29 22:24:49 +0200

    Create readBigEndianUint16 function

--- pkg/flac/bit_utils.go
+++ pkg/flac/bit_utils.go
@@ -2,6 +2,10 @@ package flac
 
 import "encoding/binary"
 
+func readBigEndianUint16(bytes []byte) uint16 {
+	return binary.BigEndian.Uint16(bytes)
+}
+
 func readBigEndianUint32(bytes []byte, offset int) uint32 {
 	var number []byte
 	bytesToBeAdded := offset / 8
--- pkg/flac/stream_info.go
+++ pkg/flac/stream_info.go
@@ -1,7 +1,5 @@
 package flac
 
-import "encoding/binary"
-
 type StreamInfo struct {
 	MinimumSampleBlockSize uint16
 	MaximumSampleBlockSize uint16
@@ -18,8 +16,8 @@ type StreamInfoReader interface {
 }
 
 func (streamInfo *StreamInfo) readStreamInfo(streamInfoData []byte) {
-	streamInfo.MinimumSampleBlockSize = binary.BigEndian.Uint16(streamInfoData[0:2])
-	streamInfo.MaximumSampleBlockSize = binary.BigEndian.Uint16(streamInfoData[2:4])
+	streamInfo.MinimumSampleBlockSize = readBigEndianUint16(streamInfoData[0:2])
+	streamInfo.MaximumSampleBlockSize = readBigEndianUint16(streamInfoData[2:4])
 
 	streamInfo.MinimumFrameSize = readBigEndianUint32(streamInfoData[4:7], 8)
 	streamInfo.MaximumFrameSize = readBigEndianUint32(streamInfoData[7:10], 8)
