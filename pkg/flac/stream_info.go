3830ed617c221f95b9ab19c5bef54ef9d16a1888
Collin <collin.busschots4@gmail.com>
2020-03-29 21:55:15 +0200

    Move stream info reader to stream info

--- pkg/flac/stream_info.go
+++ pkg/flac/stream_info.go
@@ -1,5 +1,7 @@
 package flac
 
+import "encoding/binary"
+
 type StreamInfo struct {
 	MinimumSampleBlockSize uint16
 	MaximumSampleBlockSize uint16
@@ -9,4 +11,22 @@ type StreamInfo struct {
 
 func NewStreamInfo() *StreamInfo {
 	return &StreamInfo{}
+}
+
+type StreamInfoReader interface {
+	readStreamInfo(streamInfoData []byte)
+}
+
+func (streamInfo *StreamInfo) readStreamInfo(streamInfoData []byte) {
+	streamInfo.MinimumSampleBlockSize = binary.BigEndian.Uint16(streamInfoData[0:2])
+	streamInfo.MaximumSampleBlockSize = binary.BigEndian.Uint16(streamInfoData[2:4])
+
+	streamInfo.MinimumFrameSize = readBigEndian24BitAsUint32(streamInfoData[4:7])
+	streamInfo.MaximumFrameSize = readBigEndian24BitAsUint32(streamInfoData[7:10])
+}
+
+func readBigEndian24BitAsUint32(bytes []byte) uint32 {
+	var number = []byte{0x00}
+	number = append(number, bytes...)
+	return binary.BigEndian.Uint32(number)
 }
