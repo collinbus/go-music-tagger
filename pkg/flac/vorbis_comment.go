5fc033a57e0e1a999ca7c7421f2fc5c30b3c21e9
Collin <collin.busschots4@gmail.com>
2020-03-31 22:12:06 +0200

    Test number of comments in Vorbis comment

--- pkg/flac/vorbis_comment.go
+++ pkg/flac/vorbis_comment.go
@@ -6,8 +6,10 @@ import (
 )
 
 type VorbisComment struct {
-	BlockInfo *BlockInfo
-	Vendor    string
+	BlockInfo        *BlockInfo
+	Vendor           string
+	NumberOfComments int
+	Comments         []string
 }
 
 func NewVorbisComment(blockInfo *BlockInfo) *VorbisComment {
@@ -15,7 +17,24 @@ func NewVorbisComment(blockInfo *BlockInfo) *VorbisComment {
 }
 
 func (vc *VorbisComment) Read(data []byte) {
-	vendorLength := binary.LittleEndian.Uint32(data[:4])
-	buffer := bytes.NewBuffer(data[4:vendorLength])
+	const sizeOffset = 4
+
+	vendorLength := binary.LittleEndian.Uint32(data[:sizeOffset])
+	buffer := bytes.NewBuffer(data[sizeOffset : vendorLength+sizeOffset])
 	vc.Vendor = buffer.String()
+	length := binary.LittleEndian.Uint32(data[sizeOffset+vendorLength : vendorLength+2*sizeOffset])
+	vc.NumberOfComments = int(length)
+
+	vc.readComments(data[2*sizeOffset+vendorLength:])
+}
+
+func (vc *VorbisComment) readComments(data []byte) {
+	index := 0
+	for i := 0; i < vc.NumberOfComments; i++ {
+		length := binary.LittleEndian.Uint32(data[index : index+4])
+		start := index + 1
+		index += int(length) + 4
+		buffer := bytes.NewBuffer(data[start:index])
+		vc.Comments = append(vc.Comments, buffer.String())
+	}
 }
--- pkg/flac/vorbis_comment_test.go
+++ pkg/flac/vorbis_comment_test.go
@@ -8,18 +8,29 @@ import (
 func readVorbisCommentDataFromFile() (*VorbisComment, []byte) {
 	vorbisComment := &VorbisComment{}
 	data, _ := ioutil.ReadFile(filePath)
-	blockInfo := &BlockInfo{length: 608, startIndex: 1813, isLastBlock: false}
+	blockInfo := &BlockInfo{length: 1813, startIndex: 608, isLastBlock: false}
 	vorbisComment.BlockInfo = blockInfo
 	return vorbisComment, data[608:1813]
 }
 
 func TestVendorInVorbisComment(t *testing.T) {
-	expectedVendor := "reference libFLAC 1.2.1 2007"
+	expectedVendor := "reference libFLAC 1.2.1 20070917"
 	vorbisComment, data := readVorbisCommentDataFromFile()
 
 	vorbisComment.Read(data)
 
 	if vorbisComment.Vendor != expectedVendor {
 		t.Errorf("Expected vendor %s, but is %s", expectedVendor, vorbisComment.Vendor)
+	}
+}
+
+func TestNumberOfCommentsInVorbisComment(t *testing.T) {
+	expectedNumberOfConmments := 12
+	vorbisComment, data := readVorbisCommentDataFromFile()
+
+	vorbisComment.Read(data)
+
+	if vorbisComment.NumberOfComments != expectedNumberOfConmments {
+		t.Errorf("Expected number of comments %d, but is %d", expectedNumberOfConmments, vorbisComment.NumberOfComments)
 	}
 }
