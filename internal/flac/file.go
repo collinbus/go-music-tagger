c28d3e344e2ddc0f94336a32692c59cf1450727d
Collin <collin.busschots4@gmail.com>
2020-04-09 12:39:23 +0200

    Move file struct to separate file

---
+++ internal/flac/file.go
@@ -0,0 +1,23 @@
+package flac
+
+var flacFileIdentifier = []byte{0x66, 0x4C, 0x61, 0x43}
+
+type File struct {
+	StreamInfo     *StreamInfo
+	SeekTable      *SeekTable
+	VorbisComment  *VorbisComment
+	Picture        []Picture
+	Size           int
+	audioDataStart int
+	AudioData      []byte
+}
+
+func NewFile(size int) *File {
+	return &File{Size: size, Picture: []Picture{}}
+}
+
+type BlockInfo struct {
+	startIndex  int
+	length      uint32
+	isLastBlock bool
+}
