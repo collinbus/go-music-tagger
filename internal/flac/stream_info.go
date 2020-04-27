ea9ba9e5a3787f9e9747f0f89e644f61c5676d8a
Collin <collin.busschots4@gmail.com>
2020-04-27 13:19:19 +0200

    Remove unused function

--- internal/flac/stream_info.go
+++ internal/flac/stream_info.go
@@ -34,12 +34,6 @@ func (streamInfo *StreamInfo) Read(data []byte) {
 	streamInfo.AudioDataMD5Hash = data[18:34]
 }
 
-func (streamInfo *StreamInfo) WriteBlockHeader() []byte {
-	var blockLength = make([]byte, 4)
-	binary.BigEndian.PutUint32(blockLength, streamInfo.BlockInfo.length)
-	return blockLength
-}
-
 func (streamInfo *StreamInfo) WriteStreamInfoBlock() []byte {
 	streamInfoBlockLength := streamInfo.BlockInfo.length
 	blockHeader := WriteBlockHeader(false, StreamInfoBlock, streamInfoBlockLength)
