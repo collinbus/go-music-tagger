42a6cc936a496b56afc2efb71b6ef0480547a21e
Collin <collin.busschots4@gmail.com>
2020-04-27 12:37:13 +0200

    Write MetaData BlockHeader

--- internal/flac/meta_data.go
+++ internal/flac/meta_data.go
@@ -1,5 +1,13 @@
 package flac
 
+import "encoding/binary"
+
+type BlockType int
+
+const (
+	StreamInfoBlock BlockType = 0
+)
+
 type MetaDataReader interface {
 	Read(data []byte)
 }
@@ -12,4 +20,11 @@ type BlockInfo struct {
 
 func NewBlockInfo(startIndex int, length uint32, isLastBlock bool) *BlockInfo {
 	return &BlockInfo{startIndex: startIndex, length: length, isLastBlock: isLastBlock}
+}
+
+func WriteBlockHeader(isLastBlock bool, blockType BlockType, length uint32) []byte {
+	var blockLength = make([]byte, 4)
+	blockLength[0] = byte(blockType)
+	binary.BigEndian.PutUint32(blockLength, length)
+	return blockLength
 }
---
+++ internal/flac/meta_data_test.go
@@ -0,0 +1,18 @@
+package flac
+
+import "testing"
+
+func TestWriteMetaDataBlockHeader(t *testing.T) {
+	expectedHeader := []byte{0x00, 0x00, 0x00, 0x22}
+	header := WriteBlockHeader(false, StreamInfoBlock, 34)
+
+	if header == nil {
+		t.Errorf("The header should never be nil")
+	}
+
+	for i, b := range header {
+		if b != expectedHeader[i] {
+			t.Errorf("byte at index %d should be %d but was %d", i, expectedHeader[i], b)
+		}
+	}
+}
