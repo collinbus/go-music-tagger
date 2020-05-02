dfa5ca55701c330a3143244fb9584d3b4123e2b6
Collin <collin.busschots4@gmail.com>
2020-05-02 14:19:21 +0200

    Extract methods for reading picture data

--- internal/flac/picture.go
+++ internal/flac/picture.go
@@ -24,36 +24,46 @@ func (p *Picture) Read(data []byte) {
 
 	p.PictureType = readBigEndianUint32(data[index:end], 0)
 
-	index = end
-	end = index + 4
-	mimeTypeLength := readBigEndianUint32(data[index:end], 0)
-	index = end
-	end = index + int(mimeTypeLength)
-	p.MimeType = bytes.NewBuffer(data[index:end]).String()
+	index, end = p.updateMimeType(index, end, data)
 
-	index = end
-	end = index + 4
-	descriptionLength := readBigEndianUint32(data[index:end], 0)
-	index += 4
-	end = index + int(descriptionLength)
-	p.Description = bytes.NewBuffer(data[index:end]).String()
+	index, end = p.updateDescription(index, end, data)
 
-	index = end
-	end += 4
+	index, end = p.updateIndexForNextBlock(index, end)
 	p.Width = readBigEndianUint32(data[index:end], 0)
 
-	index = end
-	end += 4
+	index, end = p.updateIndexForNextBlock(index, end)
 	p.Height = readBigEndianUint32(data[index:end], 0)
 
-	index = end
-	end += 4
+	index, end = p.updateIndexForNextBlock(index, end)
 	p.ColorDepth = readBigEndianUint32(data[index:end], 0)
 
-	index = end
-	end += 4
+	index, end = p.updateIndexForNextBlock(index, end)
 	p.IndexedColorPictures = readBigEndianUint32(data[index:end], 0)
 
 	index += 8
 	p.PictureData = data[index:]
+}
+
+func (p *Picture) updateMimeType(index int, end int, data []byte) (int, int) {
+	index, end = p.updateIndexForNextBlock(index, end)
+	mimeTypeLength := readBigEndianUint32(data[index:end], 0)
+	index = end
+	end = index + int(mimeTypeLength)
+	p.MimeType = bytes.NewBuffer(data[index:end]).String()
+	return index, end
+}
+
+func (p *Picture) updateDescription(index int, end int, data []byte) (int, int) {
+	index, end = p.updateIndexForNextBlock(index, end)
+	descriptionLength := readBigEndianUint32(data[index:end], 0)
+	index += 4
+	end = index + int(descriptionLength)
+	p.Description = bytes.NewBuffer(data[index:end]).String()
+	return index, end
+}
+
+func (p *Picture) updateIndexForNextBlock(index int, end int) (int, int) {
+	index = end
+	end = index + 4
+	return index, end
 }
